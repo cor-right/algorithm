e62de28e05de457146048657929900c7134d20a4
forgetBug <2448685828@qq.com>
2018-05-02 20:18:49 +0800

    Two leetcode data structure practice as a start.

---
+++ src/main/java/leetcode/_20180502/_01.java
@@ -0,0 +1,44 @@
+package leetcode._20180502;
+
+
+/**
+ * Created by Jiaxu_Zou on 2018-5-2
+ *
+ * Title :
+ *     minimum-depth-of-binary-tree
+ *
+ * Description:
+ *     Given a binary tree, find its minimum depth. The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ *
+ * Key Word :
+ *     Tree
+ *
+ * Note :
+ *     递归
+ *     这里需要注意题目的说明，最小深度指的是距离最近叶节点的距离， 所以要注意对空节点的判断，这里之前掉进了坑里
+ */
+public class _01 {
+
+    // tree node define
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+    public int run(TreeNode root) {
+        if (root == null)   // root == null
+            return 0;
+        else if (root.left == null && root.right == null)   // left child == right child == null
+            return 1;
+        else if (root.left == null || root.right == null)   // left child == null || right == null
+            return root.left == null ? run(root.right) + 1: run(root.left) + 1;
+        else    // left chile != null && right child != null
+            return Math.min(run(root.left), run(root.right)) + 1;
+    }
+
+
+
+}
---
+++ src/main/java/leetcode/_20180502/_02.java
@@ -0,0 +1,58 @@
+package leetcode._20180502;
+
+import java.util.Stack;
+
+/**
+ * Created by Jiaxu_Zou on 2018-5-2
+ *
+ * Title :
+ *     evaluate-reverse-polish-notation
+ *
+ * Description :
+ *     Evaluate the value of an arithmetic expression in Reverse Polish Notation.\
+ *     Valid operators are+,-,*,/. Each operand may be an integer or another expression.
+ *
+ * Examples :
+ *     ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ *     ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ *
+ * Key Word :
+ *     Stack
+ *
+ * Note :
+ *     无非是利用栈的原理模拟运算
+ */
+public class _02 {
+
+    public int evalRPN(String[] tokens) {
+        // check
+        if (tokens == null || tokens.length <= 0)
+            return 0;
+        // main
+        Stack<Integer> digits = new Stack<Integer>();
+        int pointer = 0;
+        while (pointer < tokens.length) {
+            try {
+                int digit = Integer.parseInt(tokens[pointer]);
+                // is digit, push in
+                digits.push(digit);
+            } catch(Exception ex) {
+                // is operator, calculate and push
+                int digitB = digits.pop();  // last digit
+                int digitA = digits.pop();  // first digit
+                if (tokens[pointer].trim().equals("+")) {
+                    digits.push(digitA + digitB);
+                } else if (tokens[pointer].trim().equals("-")) {
+                    digits.push(digitA - digitB);
+                } else if (tokens[pointer].trim().equals("*")) {
+                    digits.push(digitA * digitB);
+                } else if (tokens[pointer].trim().equals("/")) {
+                    digits.push(digitA / digitB);
+                }
+            }
+            pointer++;
+        }
+        return digits.pop();
+    }
+
+}
