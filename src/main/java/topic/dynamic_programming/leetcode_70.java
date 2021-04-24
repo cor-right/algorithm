f4fb68880f648b15c292d9a54a5c0eef0d308a29
cor_right <2448685828@qq.com>
2021-04-24 22:18:26 +0800

    70

---
+++ src/main/java/topic/dynamic_programming/leetcode_70.java
@@ -0,0 +1,36 @@
+package topic.dynamic_programming;
+
+public class leetcode_70 {
+
+    public int climbStairs(int n) {
+
+        int[] stepCountArr = new int[n + 2];
+
+        stepCountArr[0] = 0;
+        stepCountArr[1] = 1;
+        stepCountArr[2] = 2;
+
+        //
+
+        for (int i  = 3; i <= n; i++) {
+            stepCountArr[i] = calStepCount(i, stepCountArr);
+        }
+
+        return stepCountArr[n - 1];
+    }
+
+    private int calStepCount(int n, int[] stepCountArr) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        return stepCountArr[n - 1] + stepCountArr[n - 2];
+    }
+
+}
