317cd124acd64ee15daef13a942a2e24e5148352
佳叙 <2448685828@qq.com>
2022-12-02 01:24:16 +0800

    1201打卡

---
+++ src/main/java/offer/day12/Offer25.java
@@ -0,0 +1,94 @@
+package offer.day12;
+
+/**
+ * @code https://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/?envType=study-plan&id=lcof&plan=lcof&plan_progress=108mt86
+ *
+ * 剑指 Offer 25. 合并两个排序的链表
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 1000
+ *
+ */
+public class Offer25 {
+
+    public class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) { val = x; }
+    }
+
+    /**
+     * 模拟
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+
+        ListNode head = null;
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        ListNode cursor = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cursor.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cursor.next = l2;
+                l2 = l2.next;
+            }
+            cursor = cursor.next;
+        }
+
+        if (l1 != null) {
+            cursor.next = l1;
+        }
+        if (l2 != null) {
+            cursor.next = l2;
+        }
+
+        return head;
+    }
+
+    /**
+     * 递归
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoList(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoList(l1, l2.next);
+            return l2;
+        }
+    }
+
+}
